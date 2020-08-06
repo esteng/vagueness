683758830ebdd52d00c174860b6655c1df84495c
Jimena Guallar-Blasco <jgualla1@login.clsp.jhu.edu>
2020-08-06 17:35:54 -0400

    New files

--- src/VQA_Data/Predicate_Search_Job.sh
+++ src/VQA_Data/Predicate_Search_Job.sh
@@ -2,12 +2,12 @@
 
 #$ -j yes
 #$ -N predicate_search_job
-#$ -o /home/jgualla1/vagueness/src/VQA_Data/regex_dict.json
+#$ -o /home/jgualla1/vagueness/src/VQA_Data/output_old_csv.csv
 #$ -l 'mem_free=1M,h_rt=01:00:00'
 #$ -m ae -M jgualla1@jh.edu
 #$ -cwd
 
-python /home/jgualla1/vagueness/src/VQA_Data/VQA_Predicate_Search.py
+python /home/jgualla1/vagueness/src/VQA_Data/CSV_Creator.py
 
 
 
